533218b9353148f929d12598000498fd60a913c5
Alexey Selivanov <lazypacman@gmail.com>
2023-01-01 12:04:23 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="m-4">
+    <ul class="list-disc list-inside">
+      <li>
+        <a href="https://artialex.github.io" target="_blank">Mind Garden</a>
+      </li>
+      <li>
+        <a href="https://github.com/artialex" target="_blank">Github</a>
+      </li>
+      <li>
+        <a href="https://figma.com" target="_blank">Figma</a>
+      </li>
+    </ul>
+  </main>
+</template>
